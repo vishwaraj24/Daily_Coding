1d5af96aa1d6bfccee979333ccf3a1892b6ac4ec
vishwaraj24 <vishwarajrajebhonsle@gmail.com>
2026-08-10 19:17:04 +0530

    Add files via upload

---
+++ main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 3;
+        int sum = a + b;
+
+        System.out.println("Sum = " + sum);
+    }
+}
