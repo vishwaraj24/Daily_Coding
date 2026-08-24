de08040d0ee1a8910b8b951e43886b4d77b84f43
vishwaraj24 <vishwarajrajebhonsle@gmail.com>
2026-08-24 23:59:19 +0530

    Add files via upload

---
+++ exist.py
@@ -0,0 +1,8 @@
+# Check Key
+# Given a dictionary, check whether a particular key exists or not.
+employee={"name":"sneha","age":21,"salary":100000}
+k=input("Enter the key :")
+if k in employee :
+    print("Exist")
+else:
+    print("not exist")
